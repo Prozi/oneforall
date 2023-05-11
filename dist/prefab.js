fde13611ccd4ad752ecbefa3ba726fe839b0b560
Jacek Piętal <jpi@anixe.pl>
2023-05-11 21:42:40 +0200

    feat: change target

--- dist/prefab.js
+++ dist/prefab.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Prefab = void 0;
 const game_object_1 = require("./game-object");
@@ -16,12 +7,10 @@ class Prefab extends game_object_1.GameObject {
         super(name, 0, 0);
         this.createFunction = createFunction;
     }
-    instantiate() {
-        return __awaiter(this, void 0, void 0, function* () {
-            const gameObject = new game_object_1.GameObject(this.name, this.x, this.y);
-            yield this.createFunction(gameObject);
-            return gameObject;
-        });
+    async instantiate() {
+        const gameObject = new game_object_1.GameObject(this.name, this.x, this.y);
+        await this.createFunction(gameObject);
+        return gameObject;
     }
 }
 exports.Prefab = Prefab;
