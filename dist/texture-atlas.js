01dd884ce49701697968f125e6b0b4720ff09f3d
Jacek Piętal <jpi@anixe.pl>
2024-04-18 15:03:49 +0200

    feat: texture atlas extractor

---
+++ dist/texture-atlas.js
@@ -0,0 +1,109 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TextureAtlas = void 0;
+const PIXI = __importStar(require("pixi.js"));
+class TextureAtlas {
+    /**
+     * create texture atlas
+     * @param textureAtlasOptions
+     */
+    constructor({ texture, tileWidth, tileHeight, cols, rows, count, offset = 0, scaleMode = 'nearest' }) {
+        /**
+         * texture slices
+         */
+        this.slices = [];
+        this.texture = texture;
+        if (tileWidth && tileHeight) {
+            this.tileWidth = tileWidth;
+            this.tileHeight = tileHeight;
+        }
+        else if (cols && rows) {
+            this.tileWidth = this.width / cols;
+            this.tileHeight = this.height / rows;
+        }
+        else {
+            throw new Error('Specify tileWidth/tileHeight or cols/rows.');
+        }
+        this.offset = offset;
+        this.scaleMode = scaleMode;
+        this.preload(count);
+    }
+    /**
+     * width read from texture
+     */
+    get width() {
+        return this.texture.width;
+    }
+    /**
+     * height read from texture
+     */
+    get height() {
+        return this.texture.height;
+    }
+    /**
+     * get slice on index
+     */
+    get(frame) {
+        if (typeof headless !== 'undefined') {
+            return PIXI.Texture.WHITE;
+        }
+        if (!this.slices[frame]) {
+            this.getSlice(frame);
+        }
+        return this.slices[frame];
+    }
+    /**
+     * used internally to get slice
+     */
+    getSlice(frame) {
+        if (typeof headless !== 'undefined') {
+            return PIXI.Texture.WHITE;
+        }
+        const cols = Math.floor(this.width / this.tileWidth);
+        const index = Math.floor(frame - this.offset);
+        const x = (index % cols) * this.tileWidth;
+        const y = Math.floor(index / cols) * this.tileHeight;
+        const texture = new PIXI.Texture({
+            source: this.texture.source,
+            frame: new PIXI.Rectangle(x, y, this.tileWidth, this.tileHeight)
+        });
+        texture.source.scaleMode = this.scaleMode;
+        return texture;
+    }
+    /**
+     * used internally to preload cached slices
+     */
+    preload(count = Math.floor((this.width / this.tileWidth) * (this.height / this.tileHeight))) {
+        if (this.slices.length) {
+            throw new Error('Don\'t call prepare() twice.');
+        }
+        Array.from({ length: count }, (_, frame) => {
+            const texture = this.getSlice(frame);
+            this.slices.push(texture);
+        });
+    }
+}
+exports.TextureAtlas = TextureAtlas;
---
+++ src/texture-atlas.ts
@@ -0,0 +1,146 @@
+import * as PIXI from 'pixi.js';
+
+// declare this on server or in tests to save memory
+declare var headless: boolean;
+
+export interface TextureAtlasOptions {
+  texture: PIXI.Texture;
+  tileWidth?: number;
+  tileHeight?: number;
+  cols?: number;
+  rows?: number;
+  offset?: number;
+  count?: number;
+  scaleMode?: PIXI.SCALE_MODE;
+}
+
+export class TextureAtlas {
+  /**
+   * texture atlas base texture (required in constructor)
+   */
+  texture: PIXI.Texture;
+
+  /**
+   * calculated from cols/rows or passed in constructor
+   */
+  tileWidth: number;
+
+  /**
+   * calculated from cols/rows or passed in constructor
+   */
+  tileHeight: number;
+
+  /**
+   * get(frame) gets slice of index frame - offset
+   * if unsure leave at 0
+   */
+  offset: number;
+
+  /**
+   * scale mode for slices
+   */
+  scaleMode: PIXI.SCALE_MODE;
+
+  /**
+   * texture slices
+   */
+  slices: PIXI.Texture[] = [];
+
+  /**
+   * create texture atlas
+   * @param textureAtlasOptions
+   */
+  constructor({
+    texture,
+    tileWidth,
+    tileHeight,
+    cols,
+    rows,
+    count,
+    offset = 0,
+    scaleMode = 'nearest'
+  }: TextureAtlasOptions) {
+    this.texture = texture;
+    if (tileWidth && tileHeight) {
+      this.tileWidth = tileWidth;
+      this.tileHeight = tileHeight;
+    } else if (cols && rows) {
+      this.tileWidth = this.width / cols;
+      this.tileHeight = this.height / rows;
+    } else {
+      throw new Error('Specify tileWidth/tileHeight or cols/rows.');
+    }
+    this.offset = offset;
+    this.scaleMode = scaleMode;
+    this.preload(count);
+  }
+
+  /**
+   * width read from texture
+   */
+  get width(): number {
+    return this.texture.width;
+  }
+
+  /**
+   * height read from texture
+   */
+  get height(): number {
+    return this.texture.height;
+  }
+
+  /**
+   * get slice on index
+   */
+  get(frame: number): PIXI.Texture {
+    if (typeof headless !== 'undefined') {
+      return PIXI.Texture.WHITE;
+    }
+
+    if (!this.slices[frame]) {
+      this.getSlice(frame);
+    }
+
+    return this.slices[frame];
+  }
+
+  /**
+   * used internally to get slice
+   */
+  protected getSlice(frame: number): PIXI.Texture {
+    if (typeof headless !== 'undefined') {
+      return PIXI.Texture.WHITE;
+    }
+
+    const cols: number = Math.floor(this.width / this.tileWidth);
+    const index: number = Math.floor(frame - this.offset);
+    const x: number = (index % cols) * this.tileWidth;
+    const y: number = Math.floor(index / cols) * this.tileHeight;
+    const texture: PIXI.Texture = new PIXI.Texture({
+      source: this.texture.source,
+      frame: new PIXI.Rectangle(x, y, this.tileWidth, this.tileHeight)
+    });
+
+    texture.source.scaleMode = this.scaleMode;
+
+    return texture;
+  }
+
+  /**
+   * used internally to preload cached slices
+   */
+  protected preload(
+    count = Math.floor(
+      (this.width / this.tileWidth) * (this.height / this.tileHeight)
+    )
+  ): void {
+    if (this.slices.length) {
+      throw new Error("Don't call prepare() twice.");
+    }
+
+    Array.from({ length: count }, (_, frame) => {
+      const texture = this.getSlice(frame);
+      this.slices.push(texture);
+    });
+  }
+}
