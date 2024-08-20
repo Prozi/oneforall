f1ab61ed716ae6083c03111bab96888adc0c5a9a
Prozi (Jacek Pietal) <prozi85@gmail.com>
2024-08-20 07:53:23 +0200

    feat: bump deps

--- docs/demo/demo.bundle.js
+++ docs/demo/demo.bundle.js
@@ -9734,27 +9734,32 @@
           /**
            * separate (move away) bodies
            */
-          separate() {
+          separate(callback = utils_1.returnTrue, response = this.response) {
             (0, optimized_1.forEach)(this.all(), (body) => {
-              this.separateBody(body);
+              this.separateBody(body, callback, response);
             });
           }
           /**
-           * separate (move away) 1 body
+           * separate (move away) 1 body, with optional callback before collision
            */
-          separateBody(body) {
+          separateBody(
+            body,
+            callback = utils_1.returnTrue,
+            response = this.response
+          ) {
             if (body.isStatic || body.isTrigger) {
               return;
             }
             const offsets = { x: 0, y: 0 };
-            /**
-             * @param response
-             */
-            const addOffsets = ({ overlapV: { x, y } }) => {
-              offsets.x += x;
-              offsets.y += y;
+            const addOffsets = (collision) => {
+              // optional callback
+              callback(collision);
+              if (!collision.b.isTrigger) {
+                offsets.x += collision.overlapV.x;
+                offsets.y += collision.overlapV.y;
+              }
             };
-            this.checkOne(body, addOffsets);
+            this.checkOne(body, addOffsets, response);
             if (offsets.x || offsets.y) {
               body.setPosition(body.x - offsets.x, body.y - offsets.y);
             }
